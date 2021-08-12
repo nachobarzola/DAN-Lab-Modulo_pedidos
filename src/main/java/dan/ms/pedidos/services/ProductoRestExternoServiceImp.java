fe5a65dea50bdc61eb8fa606f0f2de67b838fe50
Franco Perino <fperino@frsf.utn.edu.ar>
2021-08-12 00:05:20 -0300

    se agrego el uso de circuitbreaker-resilience4j

--- src/main/java/dan/ms/pedidos/services/ProductoRestExternoServiceImp.java
+++ src/main/java/dan/ms/pedidos/services/ProductoRestExternoServiceImp.java
@@ -2,6 +2,9 @@ package dan.ms.pedidos.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -12,34 +15,51 @@ import org.springframework.web.client.RestTemplate;
 import dan.ms.pedidos.domain.DetallePedido;
 import dan.ms.pedidos.services.interfaces.ProductoRestExternoService;
 
+
+
 @Service
 public class ProductoRestExternoServiceImp implements ProductoRestExternoService {
-
+	@Autowired
+    CircuitBreakerFactory circuitBreakerFactory;
+	
+	
 	private static String API_REST_PRODUCTO = "http://localhost:9001/";
 	private static String ENDPOINT_PRODUCTO = "api/producto";
 
     //TODO: hay que testear este metodo
 	@Override
 	public Boolean hayStockDisponible(List<DetallePedido> detP) {
+		CircuitBreaker circuitBreaker = circuitBreakerFactory.create("circuitbreaker");
+
 
 		RestTemplate restProducto = new RestTemplate();
-		String uri = API_REST_PRODUCTO + ENDPOINT_PRODUCTO;
+		String uri = API_REST_PRODUCTO + ENDPOINT_PRODUCTO +"/detallePedido";
 		//
-		ResponseEntity<Boolean> respuesta;
+		Boolean respuesta;
 
 		//
 
-		uri = uri + "/detallePedido";
 
 		HttpEntity<List<DetallePedido>> requestDetallePedido = new HttpEntity<>(detP);
-		respuesta = restProducto.exchange(uri, HttpMethod.POST, requestDetallePedido, Boolean.class);
+		//respuesta = restProducto.exchange(uri, HttpMethod.POST, requestDetallePedido, Boolean.class);
 
-		if (respuesta.getStatusCode().equals(HttpStatus.OK)) {
+		/*if (respuesta.getStatusCode().equals(HttpStatus.OK)) {
 			return respuesta.getBody();
 		} else {
 			return false;
 		}
+		*/
+		respuesta = circuitBreaker.run(
+				() -> restProducto.postForObject(uri,requestDetallePedido, Boolean.class) ,
+				throwable -> defaultResponse()
+				);
 
+		return respuesta;
 	}
+	
+	public Boolean defaultResponse() {
+		return false;
+		}
+
 
 }
